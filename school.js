6316638a8654caaa001240168751b34772db7ccf
Diksha <dikshapatidar0149@gmail.com>
2021-08-03 22:31:59 +0530

    our first project

---
+++ school.js
@@ -0,0 +1,20 @@
+var tabs = document.querySelectorAll(".navbar ul li");
+var tab_wrap = document.querySelectorAll(".tab-wrap");
+tabs.forEach(function(tab,tab_index){
+    tab.addEventListener("click",function(){
+        tabs.forEach(function (tab) {
+            tab.classList.remove("active");
+        })
+        tab.classList.add("active");
+        tab_wrap.forEach(function(content,content_index) {
+           if(content_index==tab_index) {
+               content.style.display="block";
+           }
+           else{
+            content.style.display="none";
+           }
+        })
+       
+    })
+})
+
