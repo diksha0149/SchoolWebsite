34129406ffb272cb50666e16b806b162ca84429b
Diksha <dikshapatidar0149@gmail.com>
2021-09-01 12:21:46 +0530

    succesfully completed navbar

--- school.js
+++ school.js
@@ -1,20 +1,40 @@
-var tabs = document.querySelectorAll(".navbar ul li");
-var tab_wrap = document.querySelectorAll(".tab-wrap");
-tabs.forEach(function(tab,tab_index){
-    tab.addEventListener("click",function(){
-        tabs.forEach(function (tab) {
-            tab.classList.remove("active");
-        })
-        tab.classList.add("active");
-        tab_wrap.forEach(function(content,content_index) {
-           if(content_index==tab_index) {
-               content.style.display="block";
-           }
-           else{
-            content.style.display="none";
-           }
-        })
+// var tabs = document.querySelectorAll(".navbar ul li");
+// var tab_wrap = document.querySelectorAll(".tab-wrap");
+// tabs.forEach(function(tab,tab_index){
+//     tab.addEventListener("click",function(){
+//         tabs.forEach(function (tab) {
+//             tab.classList.remove("active");
+//         })
+//         tab.classList.add("active");
+//         tab_wrap.forEach(function(content,content_index) {
+//            if(content_index==tab_index) {
+//                content.style.display="block";
+//            }
+//            else{
+//             content.style.display="none";
+//            }
+//         })
        
-    })
-})
+//     })
+// })
+// var tabs = document.querySelectorAll(".subbar1 ul li");
+// var tab_wrap = document.querySelectorAll(".tab-wrap");
+// tabs.forEach(function(tab,tab_index){
+//     tab.addEventListener("click",function(){
+//         tabs.forEach(function (tab) {
+//             tab.classList.remove("active");
+//         })
+//         tab.classList.add("active");
+//         tab_wrap.forEach(function(content,content_index) {
+//            if(content_index==tab_index) {
+//                content.style.display="block";
+//            }
+//            else{
+//             content.style.display="none";
+//            }
+//         })
+       
+//     })
+// })
+
 
